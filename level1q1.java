e119346eb2fdd5523874334ade767cd54f8674b5
Suresh955-pro <gr9301@srmist.edu.in.com>
2026-04-13 09:37:32 +0530

    week2 assignment

---
+++ level1q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class level1q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 5 == 0) {
+            System.out.println("Is the number divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number divisible by 5? No");
+        }
+    }
+
+    
+}
