dc851e401393f2e993756400da16aa2aaccfdb1f
Suresh955-pro <gr9301@srmist.edu.in.com>
2026-04-18 00:17:59 +0530

    week 4 assignment

---
+++ level1q4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class level1q4 {
+    static int checkNumber(int num) {
+        if (num > 0)
+            return 1;
+        else if (num < 0)
+            return -1;
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int result = checkNumber(num);
+
+        if (result == 1)
+            System.out.println("Positive");
+        else if (result == -1)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+
+    
+}
